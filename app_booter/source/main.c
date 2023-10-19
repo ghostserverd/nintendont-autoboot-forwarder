6a4a4082af7d45c5f52be9ade52089be414d2486
Thomas Rohloff <v10lator@myway.de>
2023-10-19 22:53:57 +0200

    Simplify app_booter one more time
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- app_booter/source/main.c
+++ app_booter/source/main.c
@@ -12,12 +12,9 @@
 typedef void (*entrypoint) (void);
 
 typedef struct {
-	u32 text_pos[7];
-	u32 data_pos[11];
-	void *text_start[7];
-	void *data_start[11];
-	u32 text_size[7];
-	u32 data_size[11];
+	u32 pos[7 + 11];
+	void *start[7 + 11];
+	u32 size[7 + 11];
 	void *bss_start;
 	u32 bss_size;
 	entrypoint entry_point;
@@ -27,24 +24,14 @@ typedef struct {
 
 void _main(void)
 {
-	for(u32 i = 0; i < 11; i++)
-	{
-		if(i < 7 && DOLHEADER->text_size[i])
+	for(u32 i = 0; i < 7 + 11; ++i)
+		if(DOLHEADER->size[i])
 		{
-			memcpy(DOLHEADER->text_start[i], ((const void *)DOLHEADER) + DOLHEADER->text_pos[i], DOLHEADER->text_size[i]);
-			sync_before_exec(DOLHEADER->text_start[i], DOLHEADER->text_size[i]);
+			memcpy(DOLHEADER->start[i], ((const void *)DOLHEADER) + DOLHEADER->pos[i], DOLHEADER->size[i]);
+			sync_before_exec(DOLHEADER->start[i], DOLHEADER->size[i]);
 		}
 
-		if(DOLHEADER->data_size[i])
-		{
-			memcpy(DOLHEADER->data_start[i], ((const void *)DOLHEADER) + DOLHEADER->data_pos[i], DOLHEADER->data_size[i]);
-			sync_before_exec(DOLHEADER->data_start[i], DOLHEADER->data_size[i]);
-		}
-	}
-
-	u8 *exeBuffer = (u8 *)(((u32)DOLHEADER->entry_point) + 8);
-	memcpy(exeBuffer, (const u8 *)ARGS_ADDR, sizeof(struct __argv));
-	sync_before_exec(exeBuffer, sizeof(struct __argv));
-
+	memcpy((u8 *)(((u32)DOLHEADER->entry_point) + 8), (const u8 *)ARGS_ADDR, sizeof(struct __argv));
+	sync_before_exec((u8 *)(((u32)DOLHEADER->entry_point) + 8), sizeof(struct __argv));
 	DOLHEADER->entry_point();
 }
