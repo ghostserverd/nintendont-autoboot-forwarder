4c7ff163b2b08f855c45b277a4046719639b90d5
Thomas Rohloff <v10lator@myway.de>
2023-10-19 11:41:07 +0200

    Use gccore for wdvd
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- source/common.h
+++ source/common.h
@@ -1,19 +1,9 @@
 # pragma once
 
-#include <ogc/ipc.h>
-#include <stdbool.h>
-#include <stdint.h>
 #include <stdlib.h>
 
-typedef int64_t s64;
-typedef uint64_t u64;
-typedef int32_t s32;
-typedef uint32_t u32;
-typedef int16_t s16;
-typedef uint16_t u16;
-typedef int8_t s8;
-typedef uint8_t u8;
+#include <gccore.h>
 
 #define mdelay(x)
 #define malloca(x, y) aligned_alloc(y, x)
-#define ALIGNED(x) __attribute__((__aligned__(x)))
+#define ALIGNED(v) ATTRIBUTE_ALIGN(v)
